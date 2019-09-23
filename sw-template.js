ccfc9c8891e8aea612ba4ba410b09322f747eb75
reuixiy <reuixiy@gmail.com>
2019-09-24 01:31:19 +0800

    fix: cache rules

--- sw-template.js
+++ sw-template.js
@@ -112,13 +112,16 @@ workbox.routing.registerRoute(
 // Videos
 workbox.routing.registerRoute(
     /^https:\/\/gateway\.pinata\.cloud\/ipns\/io-oi\.me\/videos\/.*/,
-    new workbox.strategies.CacheOnly({
+    new workbox.strategies.CacheFirst({
         cacheName: "videos",
         plugins: [
-            new workbox.cacheableResponse.Plugin({
-                statuses: [200]
+            new workbox.expiration.Plugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
             }),
-            new workbox.rangeRequests.Plugin()
+            new workbox.cacheableResponse.Plugin({
+                statuses: [0, 200]
+            })
         ]
     })
 );
